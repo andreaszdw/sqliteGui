7a3598d89b3510f23c36d12a7ea057cb34b40257
andreaszdw <andreaszdw@googlemail.com>
2017-03-01 15:30:36 +0100

    More ui
    Added ini file, and db select

--- sqliteGuiMain.py
+++ sqliteGuiMain.py
@@ -13,37 +13,58 @@ main    : andreaszdw@googlemail.com
 
 
 import Tkinter as tk
-import sqlite3
-import tkFileDialog 
+import ttk
+import tkFileDialog
+
+import os.path
+import pickle
 
 #-----------------------------------------------------------------------------#
-class MainWindow(tk.Frame):
+class MainWindow(ttk.Frame):
     
     #-----------------------------------------------------#
     def __init__(self, parent):
-        tk.Frame.__init__(self, parent)   
-         
-        self.parent = parent        
+
+        ttk.Frame.__init__(self, parent)
+
+        # look for ini-file, and use its values, else make new ini-file
+        if os.path.exists("ini"):
+
+            # ini informations laden
+            self.uiConfig = pickle.load(open("ini", "rb"))
+
+        else:
+            # dict for ini informations
+            self.uiConfig = {"width": 800,
+                        "height": 400,
+                        "x": 300,
+                        "y": 300,
+                        "db": ""}
+
+        self.uiConfig["db"] = os.path.basename(self.uiConfig["db"])
+        self.parent = parent
+
+        self.parent.geometry("%sx%s" % (self.uiConfig["width"], self.uiConfig["height"]))
         self.initUI()
-        self.db = ""
+        self.db = self.uiConfig["db"]
+
+        # binding for changing size, save info in ini
+        self.bind("<Configure>", self.configure)
         
     #-----------------------------------------------------#
     def initUI(self):
       
-        self.parent.title("File dialogs")
+        self.parent.title("sqliteGui")
+        self.makeMenu()
         self.pack(fill=tk.BOTH, expand=1)
         
-        self.makeMenu()
-
-        '''menubar = tk.Menu(self.parent)
-        self.parent.config(menu=menubar)
-        
-        fileMenu = tk.Menu(menubar)
-        fileMenu.add_command(label="Open", command=self.onOpen)
-        menubar.add_cascade(label="File", menu=fileMenu)        '''
         
         self.txt = tk.Text(self)
         self.txt.pack(fill=tk.BOTH, expand=1)
+
+        # statusbar
+        self.statusBar = tk.Label(self, text="Datenbank: %s" % self.uiConfig["db"], bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.statusBar.pack(fill=tk.BOTH, expand=1)
 
     #-----------------------------------------------------#
     def makeMenu(self):
@@ -52,15 +73,46 @@ class MainWindow(tk.Frame):
         self.parent.config(menu=menubar)
         
         fileMenu = tk.Menu(menubar)
-        fileMenu.add_command(label="Open", command=self.onOpen)
-        menubar.add_cascade(label="File", menu=fileMenu)  
+        fileMenu.add_command(label="Select DB", command=self.selectDB)
+        fileMenu.add_command(label="Exit", command=self.exit)
+
+        menubar.add_cascade(label="File", menu=fileMenu)
+
+    #-----------------------------------------------------#
+    def configure(self, event):
+
+        self.uiConfig["width"] = event.width
+        self.uiConfig["height"] = event.height
+
+        # save info in ini-file
+        pickle.dump(self.uiConfig, open("ini", "wb"))
             
     #-----------------------------------------------------#
-    def selectDB(self, filename):
+    def selectDB(self):
 
-        f = open(filename, "r")
-        text = f.read()
-        return text
+        select = False
+
+        while not select:
+
+            file = os.path.basename(tkFileDialog.askopenfilename(title="Datenbank auswählen"))
+
+            if file != "":
+
+                self.DB = file
+                select = True
+
+                self.statusBar["text"] = "Datenbank: %s" % os.path.basename(self.DB)
+
+                self.uiConfig["DB"] = self.DB
+
+                pickle.dump(self.uiConfig, open("ini", "wb"))
+
+    #-----------------------------------------------------#
+    def exit(self):
+
+        print ("exit")
+
+        self.quit()
 
 #-----------------------------------------------------------------------------#
 if __name__ == '__main__':
