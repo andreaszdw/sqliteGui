22bb5c2dd2737426c2e8c53c01fe91a0420b114b
andreaszdw <andreaszdw@googlemail.com>
2017-03-02 09:22:19 +0100

    just for testing

--- sqliteGuiMain.py
+++ sqliteGuiMain.py
@@ -55,7 +55,7 @@ class MainWindow(ttk.Frame):
     #-----------------------------------------------------#
     def initUI(self):
       
-        self.parent.title("sqlite-Gui")
+        self.parent.title("sqlite Gui")
         self.makeMenu()
         self.pack(fill=tk.BOTH, expand=1)
         
