4677b67756c0da79de5656191a237185e0f63568
andreaszdw <andreaszdw@googlemail.com>
2017-03-07 08:28:20 +0100

    I don't no what to write

--- sqliteGuiMain.py
+++ sqliteGuiMain.py
@@ -24,6 +24,7 @@ import sqlite3
 
 #-----------------------------------------------------------------------------#
 class MainWindow(ttk.Frame):
+
     
     #-----------------------------------------------------#
     def __init__(self, parent):
@@ -57,6 +58,11 @@ class MainWindow(ttk.Frame):
 
         #for the connection
         self.conn = ""
+
+        if self.db != "":
+
+            self.makeTreeView()
+
         
     #-----------------------------------------------------#
     def initUI(self):
@@ -69,20 +75,16 @@ class MainWindow(ttk.Frame):
         pw = ttk.Panedwindow(self, orient=tk.HORIZONTAL)
         pw.pack(fill=tk.BOTH, expand=1)
 
+        # left side treeview for db
         self.dbTree = ttk.Treeview(pw)
-        #tree["columns"]=("one","two")
-        #tree.column("one", width=100 )
-        #tree.column("two", width=100)
-        #tree.heading("one", text="coulmn A")
-        #tree.heading("two", text="column B")
-        #self.dbTree.insert("" , 0,    text="Line 1")#, values=("1A","1b"))
-        #id2 = self.dbTree.insert("", 1, "dir2", text="Dir 2")
-        #self.dbTree.insert(id2, "end", "dir 2", text="sub dir 2")#, values=("2A","2B"))
-        self.dbTree.insert("", 0, text="No db in use")
-        #self.dbTree.pack()
+        self.dbTree.heading("#0", text="DB in use:")
+        self.dbTree.insert("", 0, text="None")
 
         pw.add(self.dbTree)
 
+        s = ttk.Style()
+        print s.theme_names()
+        # right side, to be set
         right = tk.Label(pw, text="right")
         pw.add(right)
 
@@ -98,6 +100,7 @@ class MainWindow(ttk.Frame):
         self.statusBar = tk.Label(self, text="Datenbank: %s" % self.uiConfig["db"], bd=1, relief=tk.SUNKEN, anchor=tk.W)
         self.statusBar.pack(fill=tk.BOTH)'''
 
+
     #-----------------------------------------------------#
     def makeMenu(self):
 
@@ -110,6 +113,7 @@ class MainWindow(ttk.Frame):
 
         menubar.add_cascade(label="File", menu=fileMenu)
 
+
     #-----------------------------------------------------#
     def configure(self, event):
 
@@ -118,7 +122,8 @@ class MainWindow(ttk.Frame):
 
         # save info in ini-file
         pickle.dump(self.uiConfig, open("ini", "wb"))
-            
+    
+
     #-----------------------------------------------------#
     def selectDB(self):
 
@@ -130,64 +135,64 @@ class MainWindow(ttk.Frame):
 
             if file != "":
 
-                # empty the tree
-                for i in self.dbTree.get_children():
-                    self.dbTree.delete(i)
-
-
                 self.db = file
                 select = True
 
-                # insert the db name as toplevel in tree
-                self.dbTree.insert("", 0, self.db, text=self.db)
-
-                #self.statusBar["text"] = "Datenbank: %s" % os.path.basename(self.db)
-
-                #save in ini
-                self.uiConfig["db"] = self.db
-                pickle.dump(self.uiConfig, open("ini", "wb"))
-
-                # connect to db
-                self.conn = sqlite3.connect(self.db)
-
-                # get cursor
-                c = self.conn.cursor()
-
-                # get the table names
-                c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
-                result = c.fetchall()
-
-                tmpCount = 0
-                for r in result:
-
-                    tmpCount += 1
-                    # insert every table as child in tree
-                    tmpChild = self.dbTree.insert(self.db, tmpCount, text=r)
-
-                    # get the table_info
-                    c.execute("PRAGMA table_info(%s)" % r)
-                    childs = c.fetchall()
-
-                    tmpCounti = 0
-                    for ch in childs:
-                        tmpCounti += 1
-                        tmpChildi = self.dbTree.insert(tmpChild, tmpCounti, text=ch[1])
+            self.makeTreeView()
 
 
+    #-----------------------------------------------------#
+    def makeTreeView(self):
 
-                    print r
-                    print ""
+        # empty the tree
+        for i in self.dbTree.get_children():
+            self.dbTree.delete(i)
 
-                    c.execute("PRAGMA table_info(%s)" % r)
+        # insert the db name as toplevel in tree
+        self.dbTree.insert("", 0, self.db, text=self.db)
 
-                    print c.fetchall()
+        #self.statusBar["text"] = "Datenbank: %s" % os.path.basename(self.db)
 
+        #save in ini
+        self.uiConfig["db"] = self.db
+        pickle.dump(self.uiConfig, open("ini", "wb"))
+
+        # connect to db
+        self.conn = sqlite3.connect(self.db)
+
+        # get cursor
+        c = self.conn.cursor()
+
+        # get the table names
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+        result = c.fetchall()
+
+        tmpCount = 0
+        for r in result:
+
+            tmpCount += 1
+            # insert every table as child in tree
+            tmpChild = self.dbTree.insert(self.db, tmpCount, text=r)
+
+            # get the table_info
+            c.execute("PRAGMA table_info(%s)" % r)
+            childs = c.fetchall()
+
+            tmpCounti = 0
+            for ch in childs:
+                tmpCounti += 1
+                tmpChildi = self.dbTree.insert(tmpChild, tmpCounti, text=ch[1])
+
+        print sqlite3.version
+        print sqlite3.version_info
+        print sqlite3.sqlite_version
 
 
     #-----------------------------------------------------#
     def exit(self):
 
         self.quit()
+
 
 #-----------------------------------------------------------------------------#
 if __name__ == '__main__':
